6460f2ce900c42195236ca5e96a5ba99b615f93b
Saumya0206 <saumyaaaaa0206@gmail.com>
2020-10-20 22:49:38 +0530

    added code

---
+++ dentist/website/urls.py
@@ -0,0 +1,11 @@
+# from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about.html', views.about, name="about"),
+    path('pricing.html', views.pricing, name="pricing"),
+    path('service.html', views.service, name="service"),
+    path('contact.html', views.contact, name="contact"),
+]
